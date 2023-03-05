594d1170a30949f1b3c0258acf5887a3771b9880
eisikogl <calion@DESKTOP-OAR9U2E.localdomain>
2023-03-06 02:45:12 +0300

    maincahnge

--- cpp1/ex03/main.cpp
+++ cpp1/ex03/main.cpp
@@ -5,7 +5,14 @@
 int main()
 {
 
-
+    {
+        Weapon club = Weapon("crude spiked club");
+        
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
     {
         Weapon club = Weapon("crude spiked club");
         HumanB jim("Jim");
