30019b17557e42440c4c64111e5f08dab60c38c7
eisikogl <calion@DESKTOP-T1C8PPE.localdomain>
2023-08-12 15:27:04 +0300

    changes cpp6

---
+++ cpp6/ex02/Base.cpp
@@ -0,0 +1,45 @@
+#include "Base.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <typeinfo>
+
+
+Base* generate(void) {
+    int random = rand() % 3;
+    switch (random) {
+        case 0: return new A;
+        case 1: return new B;
+        case 2: return new C;
+    }
+    return nullptr;
+}
+
+void identify(Base* p) {
+    if (dynamic_cast<A*>(p)) std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p)) std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p)) std::cout << "C" << std::endl;
+}
+
+void identify(Base& p) {
+    try 
+    {
+        dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+    }
+    catch (std::bad_cast &bc){}
+
+    try 
+    {
+        dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+    }
+    catch (std::bad_cast &bc){}
+
+    try
+    {
+        dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+    }
+    catch (std::bad_cast &bc){}
+}
+
--- cpp6/ex02/main.cpp
+++ cpp6/ex02/main.cpp
@@ -1,94 +1,13 @@
-#include <fstream>
+#include "Base.hpp"
 #include <iostream>
 
-class Base
-{
-    public:
-        virtual ~Base() {};
-};
+int main() {
+    srand(time(0)); // Seed for randomness
 
-class A: public Base {};
-class B: public Base {};
-class C: public Base {};
+    Base* obj = generate();
+    identify(obj);
+    identify(*obj);
 
-Base *generate()
-{
-    Base *classes[] =
-    {
-        new A(),
-        new B(),
-        new C()
-    };
-
-    srand(time(NULL));
-    size_t randomNumber = std::rand() % 3;
-
-    for (size_t i = 0; i < 3; i++)
-    {
-        if (randomNumber == i)
-            continue;
-        delete classes[i];
-    }
-    
-    return classes[randomNumber];
-}
-
-void identify(Base *p)
-{
-    if (dynamic_cast<A*>(p) == p)
-        std::cout << "A" << std::endl;
-    else if (dynamic_cast<B*>(p) == p)
-        std::cout << "B" << std::endl;
-    else if (dynamic_cast<C*>(p) == p)
-        std::cout << "C" << std::endl;
-    else
-        std::cerr << "Invalid type!" << std::endl;
-}
-
-void identify(Base &p)
-{
-    try
-    {
-        A &a = dynamic_cast<A&>(p);
-        (void)a;
-        std::cout << "A" << std::endl;
-    }
-    catch(const std::exception &e) { }
-
-    try
-    {
-        B &b = dynamic_cast<B&>(p);
-        (void)b;
-        std::cout << "B" << std::endl;
-    }
-    catch(const std::exception &e) { }
-
-    try
-    {
-        C &c = dynamic_cast<C&>(p);
-        (void)c;
-        std::cout << "C" << std::endl;
-    }
-    catch(const std::exception &e) { }
-}
-
-int main()
-{
-    //Pointers
-    {
-        Base *base = generate();
-        identify(base);
-        delete base;
-    }
-
-    std::cout << std::endl;
-
-    //Adresses
-    {
-        Base &base = *generate();
-        identify(base);
-        delete &base;
-    }
-
+    delete obj;
     return 0;
 }
