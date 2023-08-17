974915cea0517cbc51c58338957fb87e95b5a5b8
Enes Isikoglu <eisikogl@k1m31s05.42istanbul.com.tr>
2023-08-17 18:42:04 +0300

    changes to cpp6 ex01

--- cpp6/ex01/main.cpp
+++ cpp6/ex01/main.cpp
@@ -7,26 +7,17 @@ int main()
     originalData.b = 3.14f;
     uintptr_t serializedPtr;
     Data* deserializedPtr;
-
-    std::cout << "Created Data object at address: " << &originalData << std::endl;
+    
+    Data originalData;
+    uintptr_t serializedPtr;
+    Data* deserializedPtr;
 
     serializedPtr = Serializer::serialize(&originalData);
-
-    std::cout << "Serialized Data pointer value: " << serializedPtr << std::endl;
-
     deserializedPtr = Serializer::deserialize(serializedPtr);
 
-    std::cout << "Deserialized Data pointer at address: " << deserializedPtr << std::endl;
-
-    if (deserializedPtr == &originalData)
-    {
-        std::cout << "Deserialization successful. Pointers match." << std::endl;
-        std::cout << "Original Data: a = " << originalData.a << ", b = " << originalData.b << std::endl;
-    }
-    else
-    {
-        std::cout << "Deserialization failed. Pointers do not match." << std::endl;
-    }
+    std::cout << "im the original Pointer: " << &originalData << std::endl;
+    std::cout << "im serialized: " << serializedPtr << std::endl;
+    std::cout << "im deserilized: " << deserializedPtr << std::endl;
 
     return 0;
 }
