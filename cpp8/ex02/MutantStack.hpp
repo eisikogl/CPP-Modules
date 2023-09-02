efcc2067865001df751686ab9b66d319a84d6bc6
eisikogl <calion@DESKTOP-T1C8PPE.localdomain>
2023-09-02 22:11:06 +0300

    cpp8 changes

--- cpp8/ex02/MutantStack.hpp
+++ cpp8/ex02/MutantStack.hpp
@@ -3,14 +3,12 @@
 #include <stdexcept>
 #include <iostream>
 
-//to do reasearch typedef typename, when used with Container::iterator iterator 
 
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 public:
     typedef typename Container::iterator iterator;
-    typedef typename Container::const_iterator const_iterator;
 
     iterator begin()
     {
@@ -20,15 +18,5 @@ public:
     iterator end()
     {
         return this->c.end();  
-    }
-
-    const_iterator begin() const
-    {
-        return this->c.begin();
-    }
-
-    const_iterator end() const
-    {
-        return this->c.end();
     }
 };
