c5cd0d4272645b3139108c4377ba88cbcddb3a17
eisikogl <calion@DESKTOP-T1C8PPE.localdomain>
2023-09-07 23:04:49 +0300

    cpp8fix and cpp9continue

--- cpp9/ex02/PmergeMe.cpp
+++ cpp9/ex02/PmergeMe.cpp
@@ -10,17 +10,20 @@ PmergeMe::~PmergeMe()
 
 }
 
-void PmergeMe::sortUsingList() {
+void PmergeMe::sortUsingList() 
+{
     if (dataList_.empty()) return;
 
     std::list<int>::iterator it1, it2;
 
-    for (it1 = dataList_.begin(); it1 != dataList_.end(); ++it1) {
+    for (it1 = dataList_.begin(); it1 != dataList_.end(); ++it1) 
+    {
         int value = *it1;
         it2 = it1;
         --it2;
 
-        while (it2 != dataList_.begin() && *it2 > value) {
+        while (it2 != dataList_.begin() && *it2 > value)
+        {
             std::list<int>::iterator temp = it2;
             ++temp;
             *temp = *it2;
@@ -29,10 +32,13 @@ void PmergeMe::sortUsingList() {
 
         std::list<int>::iterator temp = it2;
         ++temp;
-        if (*it2 > value) {
+        if (*it2 > value) 
+        {
             *temp = *it2;
             *it2 = value;
-        } else {
+        } 
+        else 
+        {
             *temp = value;
         }
     }
@@ -45,24 +51,32 @@ void PmergeMe::sortUsingDeque()
 
     std::deque<int>::iterator it1, it2;
 
-    for (it1 = dataDeque_.begin() + 1; it1 != dataDeque_.end(); ++it1) {
+    for (it1 = dataDeque_.begin() + 1; it1 != dataDeque_.end(); ++it1) 
+    {
         int value = *it1;
         it2 = it1;
         --it2;
 
-        while (it2 >= dataDeque_.begin() && *it2 > value) {
+        while (it2 >= dataDeque_.begin() && *it2 > value) 
+        {
             *(it2 + 1) = *it2;
-            if (it2 != dataDeque_.begin()) {
+            if (it2 != dataDeque_.begin()) 
+            {
                 --it2;
-            } else {
+            }
+            else
+            {
                 break;
             }
         }
 
-        if (it2 >= dataDeque_.begin() && *it2 > value) {
+        if (it2 >= dataDeque_.begin() && *it2 > value) 
+        {
             *(it2 + 1) = *it2;
             *it2 = value;
-        } else {
+        } 
+        else 
+        {
             *(it2 + 1) = value;
         }
     }
--- cpp9/ex02/main.cpp
+++ cpp9/ex02/main.cpp
@@ -5,31 +5,20 @@
 
 int main(int argc, char* argv[]) {
     std::list<int> dataList;
-    std::deque<int> dataDeque;
-
-    for (int i = 1; i < argc; ++i) {
+    std::deque<int> dataDeque;    for (int i = 1; i < argc; ++i) 
+    {
         int value = atoi(argv[i]);
         dataList.push_back(value);
         dataDeque.push_back(value);
-    }
-
-    PmergeMe sorter(dataList, dataDeque);
-
-
-    struct timeval startList, endList;
+    }    PmergeMe sorter(dataList, dataDeque);    struct timeval startList, endList, startDeque, endDeque;    // Measure sorting time for list
     gettimeofday(&startList, NULL);
     sorter.sortUsingList();
     gettimeofday(&endList, NULL);
-    double durationList = (endList.tv_sec - startList.tv_sec) + (endList.tv_usec - startList.tv_usec) * 1e-6;
-
-    struct timeval startDeque, endDeque;
+    long durationListMicros = (endList.tv_sec - startList.tv_sec) * 1e6 + (endList.tv_usec - startList.tv_usec);    // Measure sorting time for deque
     gettimeofday(&startDeque, NULL);
     sorter.sortUsingDeque();
     gettimeofday(&endDeque, NULL);
-    double durationDeque = (endDeque.tv_sec - startDeque.tv_sec) + (endDeque.tv_usec - startDeque.tv_usec) * 1e-6;
-
-    std::cout << "Time taken using list: " << durationList << " seconds" << std::endl;
-    std::cout << "Time taken using deque: " << durationDeque << " seconds" << std::endl;
-
-    return 0;
+    long durationDequeMicros = (endDeque.tv_sec - startDeque.tv_sec) * 1e6 + (endDeque.tv_usec - startDeque.tv_usec);    // Display the results
+    std::cout << "Time taken using list: " << durationListMicros << " microseconds" << std::endl;
+    std::cout << "Time taken using deque: " << durationDequeMicros << " microseconds" << std::endl;    return 0;
 }
